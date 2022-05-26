1a8032ab76af369631e17a9962be1e9890515ade
Max Savelev <G-man74@Yandex.ru>
2022-05-27 00:01:08 +0500

    docker update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'a9d4d162b84e6e340263a58462b17ad9637a9c7a',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'a9d4d162b84e6e340263a58462b17ad9637a9c7a',
+            'dev_requirement' => false,
+        ),
+    ),
+);
